d3244e0ad3fb0be52a48043d2ba487ed21f5c4b4
unknown <kuzma.boh@gmail.com>
2019-12-02 18:49:57 +0100

    fetching first data from external API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import React, { useEffect, useState } from 'react';
+import Recipe from "./components/Recipe/Recipe"
 
 
 import './App.css';
@@ -9,11 +10,17 @@ const App = () => {
 
   const APP_ID = "d6cbc94d"
   const KEY = "ab92dfb681356353ec97d41bf84211ec"
-  const example = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${KEY}`
+  const [ recipe, setRecipe ] = useState([])
 
   useEffect( () => {
-    console.log("Effect used")
-  })
+    fetchfood()
+  }, [])
+
+  const fetchfood = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${KEY}`)
+    const data = await response.json()
+    setRecipe(data.hits)
+  }
 
   return(
     <div className="App">
@@ -21,6 +28,10 @@ const App = () => {
         <input type="text" className="form"></input>
         <button className="search-button">Search</button>
       </form>
+
+      {recipe.map(recipe => (
+        <Recipe />
+      ))}
     </div>
 
   )
