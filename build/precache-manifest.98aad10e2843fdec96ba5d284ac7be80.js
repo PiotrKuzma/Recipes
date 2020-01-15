bf00b950b2c7c29dc0ba2c652d9eb200c2116832
unknown <kuzma.boh@gmail.com>
2020-01-15 13:06:09 +0100

    Production

---
+++ build/precache-manifest.98aad10e2843fdec96ba5d284ac7be80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57654953bcb24d9f2c50b09b425b3e73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fba83b9eb95b349ee074",
+    "url": "/static/css/main.cc907e5b.chunk.css"
+  },
+  {
+    "revision": "ac9c48905d359d17cb25",
+    "url": "/static/js/2.3bacdb9e.chunk.js"
+  },
+  {
+    "revision": "fba83b9eb95b349ee074",
+    "url": "/static/js/main.57eeb79e.chunk.js"
+  },
+  {
+    "revision": "d38392ae5330a6157d0b",
+    "url": "/static/js/runtime~main.fa463cfa.js"
+  },
+  {
+    "revision": "432ce6e312f12c1bde892459cb34321b",
+    "url": "/static/media/por.432ce6e3.jpg"
+  },
+  {
+    "revision": "8496089be29dc8784db3951dde0082a3",
+    "url": "/static/media/spoon-knife.8496089b.svg"
+  }
+]);
